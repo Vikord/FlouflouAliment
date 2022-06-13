7281d79b89999b21a6c91f4e834235dc9c73340b
vince <theinvinceable@hotmail.com>
2022-06-13 18:54:56 -0400

    Nouvelles fenetres

--- FlouPizza/src/control/Main.java
+++ FlouPizza/src/control/Main.java
@@ -4,25 +4,26 @@
  */
 package control;
 
-import modele.CategorieEmploye;
-
-import java.util.HashMap;
+import ui.FenSaisie;
 
 public class Main {
     public static void main(String[] args) {
-//        java.awt.EventQueue.invokeLater(() -> {
-//            new ().setVisible(true); quelle fenêtre? pas FenSaisie
-//        });
-
-    HashMap<String, CategorieEmploye> categories = new HashMap<>();
-
-    CategorieEmploye catJunior = new CategorieEmploye("junior", 25, 1200, 50, 0);
-    CategorieEmploye catSenior = new CategorieEmploye("senior", 40, 1600, 80, 0);
-    CategorieEmploye catSuper = new CategorieEmploye("super", 60, 3000, 150, 2000);
-
-    categories.put("junior", catJunior);
-    categories.put("senior", catSenior);
-    categories.put("super", catSuper);
-
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FenSaisie().setVisible(true);
+            }
+        });
     }
+
+//    HashMap<String, CategorieEmploye> categories = new HashMap<>();
+//
+//    CategorieEmploye catJunior = new CategorieEmploye("junior", 25, 1200, 50, 0);
+//    CategorieEmploye catSenior = new CategorieEmploye("senior", 40, 1600, 80, 0);
+//    CategorieEmploye catSuper = new CategorieEmploye("super", 60, 3000, 150, 2000);
+//
+//    categories.put("junior", catJunior);
+//    categories.put("senior", catSenior);
+//    categories.put("super", catSuper);
+//
+//    }
 }
